7a4f0995254ddb2c1143f766cbc786b2771268e5
QUANG TU <quangtu2208@gmail.com>
2019-04-19 17:39:29 +0700

    advanced-login

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_04_20_090635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
